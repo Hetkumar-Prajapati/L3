e6b1d28d8a4527ba3e195cb966aea47fa9fd34c0
hetkumar1704 <200476049@student.gerogianc.on.ca>
2022-09-23 16:27:19 -0400

    l3 express server

--- server.js
+++ server.js
@@ -3,15 +3,36 @@ const express = require('express')
 const app = express()
 
 //add http request handler
-let hello= (req,res)=>{
-    res.writeHead(200)
+let hello = (req,res)=>{
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
     res.write('hello world')
     res.end()
 }
 
+let goodbye = (req,res)=>{
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+    res.write('goodbye world')
+    res.end()
+}
+
+let index = (req,res)=>{
+    if (req.url === '/'){
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+    res.write('Home page')
+    res.end()
+    }
+    else{
+        res.writeHead(404)
+        res.write('Not found')
+        res.end()
+    }
+}
+
 //route helps requests to our handler function
-app.use(hello)
+app.use('/hello',hello)     //map hello variable to hello url
+app.use('/goodbye',goodbye)
+app.use('/',index) // used at last after trying other urls
 
 //start exress web server
-app.lison(3000)
+app.listen(3000)
 console.log('server bis running on 3000')
